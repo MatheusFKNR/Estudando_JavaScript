97b126965afac9eb79fc04ef09b489cca7c8b733
Matheus <matheusrfk10@gmail.com>
2026-09-07 17:26:33 -0300

    Adiciona novos arquivos ao repositório

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var a = "Hello World";
+let b = "Ola Mundo";
+const c = "Ola Mundo !!!";
 
+console.log(a);
 console.log(b);
+console.log(c);
 //Usar ponto e virgula sempre no final como boa pratica//
