d23575696b4dc268240dbbff548e99a67e561080
Matheus Roberto Fuckner <132947398+MatheusFKNR@users.noreply.github.com>
2026-09-23 10:11:17 -0300

    Objetos-Propriedades

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
+// Criando um objeto
+const usuario = {
+  nome: "Carlos",
+  idade: 25,
+  "tipo-de-conta": "Premium" // Chaves com hífen ou espaços exigem aspas
+};
 
-var nomes = ["joão", "Maria", "Romário"];
-var notasA = [7.0, 6.5, 9.5];
-var notasB = [8.0, 7.0, 8.5];
+// Acessando propriedades
+console.log(usuario.nome);               // "Carlos" (Notação de ponto)
+console.log(usuario["idade"]);            // 25 (Notação de colchetes)
+console.log(usuario["tipo-de-conta"]);   // "Premium" (Obrigatório usar colchetes)
 
-function media(n1,n2){
-  return(n1 + n2)/2
-}
-
-function passou(media){
-  
-  if (media > 7){
-    return "Aprovado"
-  }else{
-    return "Reprovado"
-  }
-  
-}
-
-for(var index in nomes){
-
-  let nota1 = notasA[index];
-  let nota2 = notasB[index];
-
-  let m = media(nota1, nota2);
-
-  console.log(nomes[index] +
-      " - " +
-      notasA[index] +
-      " - " +
-      notasB[index] +
-      " - " +
-      passou(m))
-}
+// Acesso dinâmico via variável
+const propriedadeDesejada = "nome";
+console.log(usuario[propriedadeDesejada]); // "Carlos"
